c7cf4180c752b32085d439aae80c9a5af376959a
anilkumar <gallaanilkumar95@gmail.com>
2021-11-13 14:43:28 +0530

    oranges added

--- vs2019 demo3/Program.cs
+++ vs2019 demo3/Program.cs
@@ -8,6 +8,7 @@ namespace vs2019_demo3
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Apples");
+            Console.WriteLine("Oranges");
         }
     }
 }
